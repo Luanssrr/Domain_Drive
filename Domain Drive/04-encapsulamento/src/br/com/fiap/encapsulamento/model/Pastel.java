719d65432468aad2297f793c97f44a370b58ff9f
Luanssrr <91630530+Luanssrr@users.noreply.github.com>
2022-04-04 18:55:27 -0300

    domain java

---
+++ Domain Drive/04-encapsulamento/src/br/com/fiap/encapsulamento/model/Pastel.java
@@ -0,0 +1,39 @@
+package br.com.fiap.encapsulamento.model;
+
+public class Pastel {
+
+	
+	 private String sabor;
+	 private double valor;
+	 private boolean pronto;
+	 
+	public String getSabor() {
+		return sabor;
+	}
+	
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	
+	public void setValor(double valor) {
+		this.valor = valor;
+	
+	}
+	
+	public boolean getPronto() {
+		return pronto;
+	}
+	
+	public void setPronto(boolean pronto) {
+		this.pronto = pronto;
+	}
+	 
+	 
+	 
+
+	
+}
---
+++ Domain Drive/04-encapsulamento/src/br/com/fiap/encapsulamento/view/Teste.java
@@ -0,0 +1,19 @@
+package br.com.fiap.encapsulamento.view;
+
+import br.com.fiap.encapsulamento.model.Pastel;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		
+		Pastel pastel = new Pastel();
+		
+		
+		
+		
+		
+		
+	}
+
+}
